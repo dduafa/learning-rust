ad0ae7985e92412c1a7c4a236c54243a831215c2
kitchenblade <reign.derek@yahoo.com>
2022-01-30 21:47:55 +0000

    feat(algorithm): implement shunting yard algorithm

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Rust Playground
 
-
+* Run Test
 rustc --test charstring_tests.rs && ./charstring_tests  
--- charstrings/charstring.rs
+++ charstrings/charstring.rs
@@ -20,3 +20,27 @@ pub fn to_lower(c: char) -> char {
     }
     return c as char;
 }
+
+
+pub fn to_upper(c: char) -> char {
+    let c = c as u8;
+
+    if c >= b'a' && c <= b'z' {
+        return (c - 32) as char;
+    }
+    return c as char;
+}
+
+
+pub fn print_chars_from_bytes(s: &String) {
+    for &item in s.as_bytes().iter()  {
+        println!("{}", item as char)
+    }
+}   
+
+
+pub fn print_chars_from_chars(s: &String) {
+    for c in s.chars() {
+        println!("{}", c)
+    }
+}  
